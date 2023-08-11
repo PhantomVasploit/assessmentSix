25fa85b5990609d99f6f5a4d3ab15e90f7699c44
Paul-Sanga <paulsanganyamawi@gmail.com>
2023-08-11 12:14:51 +0300

    frontend

--- client/app.js
+++ client/app.js
@@ -5,12 +5,33 @@ const errorMessage = document.querySelector('.error-message')
 const success = document.querySelector('.success')
 document.querySelector('#communityClubForm').addEventListener('submit', (e)=>{
     e.preventDefault()
-    let firstName = document.querySelector('#firstName').value
+    let firstName = document.querySelector('#firstName')
     let lastName = document.querySelector('#lastName')
     let email = document.querySelector('#email')
     let password = document.querySelector('#password')
-    let cohortNumber = document.querySelector('#cohortNumber')
+    let cohortNumber = document.querySelector('#number')
 
+    firstName.addEventListener('input', ()=>{
+        firstName.value = this.value
+    })
+
+    lastName.addEventListener('input', ()=>{
+        lastName.value = this.value
+    })
+
+    email.addEventListener('input', ()=>{
+        email.value = this.value
+    })
+
+    password.addEventListener('input', ()=>{
+        password.value = this.value
+    })
+
+    cohortNumber.addEventListener('input', ()=>{
+        cohortNumber.value = this.value
+    })
+
+    console.log(firstName.value);
 
     axios.post('http://127.0.0.1:3000/api/v1/register', 
     {
